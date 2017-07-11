906d48567a4416b51fb308b49547707a7c6ae01a
Hubylida <2416661692@qq.com>
2017-07-12 00:47:49 +0800

    1

---
+++ index.js
@@ -0,0 +1,76 @@
+var nav = document.querySelectorAll('.nav-bar');
+nav[0].style.borderTopColor = "#000";
+// ................
+
+var text = document.querySelectorAll('.text');
+var textValue = ["请填写您的真实姓名","请填写您的手机号码"];
+    text[0].onfocus = function (){
+        if(text[0].value == textValue[0]){
+            text[0].value = "";
+        }
+    }
+    text[0].onblur = function (){
+        if(text[0].value == ""){
+            text[0].value = textValue[0];
+        }
+    }
+    text[1].onfocus = function (){
+        if(text[1].value == textValue[1]){
+            text[1].value = "";
+        }
+    }
+    text[1].onblur = function (){
+        if(text[1].value == ""){
+            text[1].value = textValue[1];
+        }
+    }
+// ............
+
+var hoverPic  = document.querySelectorAll('.hoverPic');
+var pictureL = hoverPic.length;
+var hoverPicSrc = 
+[
+    "http://7868925.s61i.faiusr.com/2/AD0I-aPgAxACGAAg8Z69xgUo7rC07AEw7AE44gI.jpg",
+    "http://7868925.s61i.faiusr.com/2/AD0I-aPgAxACGAAg8Z69xgUo6eWlvwMw7QE44gI.jpg",
+    "http://7868925.s61i.faiusr.com/2/AD0I-aPgAxACGAAg8Z69xgUo4Kyq5gYw7AE44gI.jpg",
+    "http://7868925.s61i.faiusr.com/2/AD0I-aPgAxACGAAg8p69xgUo8IzQLzDsATjiAg.jpg"
+];
+var hoverPicSrcF = 
+[
+    "http://7868925.s61i.faiusr.com/2/AD0I-aPgAxACGAAghIzcvwUo7YCdwQIw7AE44gI.jpg",
+    "http://7868925.s61i.faiusr.com/2/AD0I-aPgAxACGAAghIzcvwUo9rz_jgUw7AE44gI.jpg",
+    "http://7868925.s61i.faiusr.com/2/AD0I-aPgAxACGAAgg4zcvwUo4sOp3wMw7AE44gI.jpg",
+    "http://7868925.s61i.faiusr.com/2/AD0I-aPgAxACGAAgg4zcvwUogPCxDzDsATjiAg.jpg"
+]
+   for(let i = 0; i < pictureL; i++){
+        hoverPic[i].addEventListener('mouseover',function(){
+                hoverPic[i].src = hoverPicSrc[i];           
+        });
+        hoverPic[i].addEventListener('mouseout',function(){
+            hoverPic[i].src = hoverPicSrcF[i];
+        });
+    }
+
+// ...........
+var go_up = document.querySelector('.go-up');
+window.onscroll = function(){
+    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    var clientHeight = document.documentElement.clientHeight;
+    if (currentScroll > clientHeight){
+        go_up.style.display = "inline-block";
+    }else{
+        go_up.style.display = "none";
+    }
+}
+go_up.addEventListener('click',scrollBack);
+var timer;
+function scrollBack(){
+    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    if(currentScroll != 0){
+        window.scrollBy(0,-50);
+        timer = setTimeout('scrollBack()',10)
+    }else {
+        clearTimeout(timer)
+    }
+}
+
